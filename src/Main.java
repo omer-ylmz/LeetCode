9623f48155b50bd3643ac7320c51f74fbebe7a6b
omer-ylmz <omer.yilmaz.gru@hotmail.com>
2024-10-24 15:24:40 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        LeetCode9 deneme = new LeetCode9();
+
+        System.out.println(deneme.isPalindrome(-121));
+
+
+    }
+
+}
